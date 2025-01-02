53aed0a46495bb7a1cecafc65ddbb22bccb711fc
haydarnt8 <haydarnt8@gmail.com>
2025-01-03 00:48:45 +0300

    Refactor button class in BaseDeleteDialog for consistent styling

--- src/components/base/BaseDeleteDialog.vue
+++ src/components/base/BaseDeleteDialog.vue
@@ -79,7 +79,7 @@ async function handleConfirm() {
               <button
                 @click="handleConfirm"
                 type="button"
-                class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                class="w-28 inline-flex justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3"
               >
                 <BaseLoadingSpinner v-if="isLoading" />
                 <span v-else> Confirm </span>

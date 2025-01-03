2b53770f6bf01f1788eabf20d8d93aefb309be55
haydarnt8 <haydarnt8@gmail.com>
2025-01-03 14:23:48 +0300

    Refactor logout button in BillsStatistics component for improved styling and remove text visibility on smaller screens

--- src/components/billsManagement/BillsStatistics.vue
+++ src/components/billsManagement/BillsStatistics.vue
@@ -53,7 +53,7 @@ function logout() {
         </button>
         <button
           @click="confirmDialog = true"
-          class="flex min-w-fit items center gap-2 px-3 py-2 text-white bg-red-500 rounded-lg shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-white"
+          class="flex min-w-fit items center gap-2 px-2 py-2 text-white bg-red-500 rounded-lg shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-white"
         >
           <svg
             class="w-6 h-6"
@@ -69,8 +69,6 @@ function logout() {
               d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
             ></path>
           </svg>
-          <!-- show the text if the screen biger than md -->
-          <span class="hidden md:block">Logout</span>
         </button>
       </div>
     </div>
